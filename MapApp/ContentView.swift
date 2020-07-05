96ea87eaee889af0372edf2853b655cf0c672f7b
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-05 18:22:17 +0900

    Initial Commit

---
+++ MapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapApp
+//
+//  Created by nhajime on 2020/07/05.
+//  Copyright © 2020 nhajime. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
